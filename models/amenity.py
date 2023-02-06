8d89e6885131c59b8a99362ae45abbf9df4ed94f
Almarat <arnualmarat@gmail.com>
2023-02-06 15:37:59 +0300

    deleted my commited files

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""amenity class"""
-from base.model import BaseModel
-
-
-class Amenity(BaseModel):
-    """amenity class"""
-    name =""
--- models/state.py
+++
@@ -1,9 +0,0 @@
-#!/user/bin/python3
-""" classes inherting from module"""
-
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ class state"""
-    name = ""
