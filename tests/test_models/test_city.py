4694c94e52b7a32a6c61bfc77a207f464acff0a6
Makuo67 <okekemakuo67@gmail.com>
2023-02-18 00:32:25 +0200

    :robot: automated commit of one or many files

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,63 +1,42 @@
 #!/usr/bin/python3
-"""
-Tests for City module
-"""
+
+"""Unittest for Amenity Class."""
 
 import unittest
-import pep8
-from datetime import datetime
-import models
+
 from models.city import City
+
+from models.base_model import BaseModel
 
 
 class TestCity(unittest.TestCase):
-    """Unit Tests"""
+    """Test cases City class."""
 
-    def setUp(self):
-        self.city = City()
+    def test_instance(self):
+        """test instance."""
+        city = City()
+        self.assertIsInstance(city, City)
 
-    def test_city_init(self):
-        """
-        1. test if a new instance of the class 'City' is created correctly
-        2. test that the 'name' attribute of the City object is an
-           instance of the str class
-        3. test if the 'state_id' attribute of the City object
-           is an instance of the str class
-        4. test if the initial value of 'state_id' attribute
-           is an empty string
-        5. test if the initial value of 'name' attribute is an empty string
-        """
-        self.assertIsInstance(self.city, City)
-        self.assertIsInstance(self.city.name, str)
-        self.assertIsInstance(self.city.state_id, str)
-        self.assertEqual(self.city.state_id, "")
-        self.assertEqual(self.city.name, "")
+    def test_is_class(self):
+        """test instance."""
+        city = City()
+        self.assertEqual(str(type(city)),
+                         "<class 'models.city.City'>")
 
-    def test_name_setter(self):
-        """
-        test if the 'name' attribute can be set to a new value correctly
-        """
-        self.city.name = "Onitsha"
-        self.assertEqual(self.city.name, "Onitsha")
+    def test_is_subclass(self):
+        """test is_subclass."""
+        city = City()
+        self.assertTrue(issubclass(type(city), BaseModel))
 
-    def test_name_getter(self):
-        """
-        test if the 'name' attribute gets the correct value
-        """
-        self.city.name = "Port Harcourt"
-        self.assertEqual(self.city.name, "Port Harcourt")
+    def test_name(self):
+        """test is_subclass."""
+        city = City()
+        self.assertEqual(city.name, "")
 
-    def test_city_pep8(self):
-        """test if city.py is PEP8 compliant"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/city.py'])
-        self.assertEqual(result.total_errors, 0, "PEP8 errors found")
-
-    def test_pep8(self):
-        """test if this file is PEP8 compliant"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['tests/test_models/test_city.py'])
-        self.assertEqual(result.total_errors, 0, "PEP8 errors found")
+    def test_state_id(self):
+        """test is_subclass."""
+        city = City()
+        self.assertEqual(city.state_id, "")
 
 
 if __name__ == "__main__":
